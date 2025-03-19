e761a13a2861e60dbeba4f93ac68edecb5d96d60
abbiebabu <abhaymaskey12@gmail.com>
2025-03-19 21:07:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -13,16 +13,34 @@
 // })
 
 
-let userScore=0;
-let compScore=0;
 
-const choices = document.querySelectorAll('.choice');
+// -----------first time -----------------
+// let userScore=0;
+// let compScore=0;
 
+// const choices = document.querySelectorAll('.choice');
+
+// choices.forEach((choice) =>{
+// choice.addEventListener("click", ()=>{
+//     const userChoice = choice.getAttribute('id');
+//     console.log('your choose', userChoice);
+    
+// })
+
+// })
+
+
+
+userScore=0;
+computerScore=0;
+
+const choices=document.querySelectorAll(".choice");
 choices.forEach((choice) =>{
-choice.addEventListener("click", ()=>{
-    const userChoice = choice.getAttribute('id');
-    console.log('your choose', userChoice);
+
+choice.addEventListener("click",()=>{
+    const usrChoice = choice.getAttribute("id");
+    console.log('you choose', usrChoice);
     
 })
 
-})
+});
